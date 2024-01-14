58619168f0ecd2c64e8fc47d898ee394e29799c9
Evie <idomin3craft@gmail.com>
2024-01-14 16:29:50 -0600

    Added data structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#![allow(dead_code)]
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Position {
+	x: f64,
+	y: f64,
+}
+
+impl Position {
+	pub fn new(x: f64, y: f64) -> Self {
+		Position { x, y }
+	}
+}
+
+#[derive(Component)]
+struct Draggable;
+
+#[derive(Component)]
+struct Jewel;
+
 fn main() {
-    println!("Hello, world!");
+	App::new()
+		.add_plugins(DefaultPlugins)
+		.add_systems(Startup, setup)
+		.run();
+}
+
+fn setup(mut commands: Commands) {
+	commands.spawn((Jewel, Position::new(0.0, 0.0)));
 }
