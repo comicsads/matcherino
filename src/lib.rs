c6346f5f2183d1b7eab2a75031fc8619d01a0f1d
Evie <idomin3craft@gmail.com>
2024-01-16 12:58:29 -0600

    Made dragging snap to line so you can only drag vertically or horizontally, not both

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,10 @@ const GRID_SIZE: f32 = GEM_SIZE;
 pub struct Draggable;
 
 #[derive(Component)]
-pub struct Dragging;
+pub struct Dragging {
+	start_x: f32,
+	start_y: f32,
+}
 
 #[derive(Component)]
 pub struct Orb;
@@ -37,7 +40,10 @@ pub fn start_drag(
 
 					match collide {
 						Some(_) => {
-							commands.entity(e_id).insert(Dragging);
+							commands.entity(e_id).insert(Dragging {
+								start_x: sprite_pos.translation.x,
+								start_y: sprite_pos.translation.y,
+							});
 							break;
 						}
 						None => (),
@@ -49,18 +55,30 @@ pub fn start_drag(
 }
 
 pub fn time_to_drag(
-	mut query: Query<(&mut Transform, Entity), With<Dragging>>,
+	mut query: Query<(&mut Transform, Entity, &Dragging)>,
 	window_query: Query<&Window>,
 	buttons: Res<Input<MouseButton>>,
 	mut commands: Commands,
 ) {
 	//TODO: This function is being run when supposedly no entities have Dragging component
-	for (mut sprite_pos, e_id) in &mut query {
+	for (mut sprite_pos, e_id, drag) in &mut query {
 		let main_window = &window_query.single();
 		if let Some(m_pos) = main_window.cursor_position() {
 			let mouse_pos_adjusted = adjust_mouse_pos(m_pos, main_window);
-			sprite_pos.translation.x = (mouse_pos_adjusted.x / GRID_SIZE).round() * GRID_SIZE;
-			sprite_pos.translation.y = (mouse_pos_adjusted.y / GRID_SIZE).round() * GRID_SIZE;
+
+			let dist_dragged_x = drag.start_x - mouse_pos_adjusted.x;
+			let dist_dragged_y = drag.start_y - mouse_pos_adjusted.y;
+
+			let dist_dragged_x = dist_dragged_x.abs();
+			let dist_dragged_y = dist_dragged_y.abs();
+
+			if dist_dragged_x > dist_dragged_y {
+				sprite_pos.translation.x = (mouse_pos_adjusted.x / GRID_SIZE).round() * GRID_SIZE;
+				sprite_pos.translation.y = drag.start_y;
+			} else {
+				sprite_pos.translation.x = drag.start_x;
+				sprite_pos.translation.y = (mouse_pos_adjusted.y / GRID_SIZE).round() * GRID_SIZE;
+			}
 		}
 		if !buttons.pressed(MouseButton::Left) {
 			commands.entity(e_id).remove::<Dragging>();
