8291368d8ac3404ea97d1550168f87c177aeb146
Evie <idomin3craft gmail>
2024-07-15 20:21:44 -0500

    Pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,15 +1,17 @@
-{ pkgs ? import <nixpkgs> { } }:
+{pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/53e81e790209e41f0c1efa9ff26ff2fd7ab35e27.tar.gz") {}}:
 
 with pkgs;
-
-mkShell rec {
-  nativeBuildInputs = [
-    pkg-config
-  ];
-  buildInputs = [
-    udev alsa-lib vulkan-loader
-    xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
-    libxkbcommon wayland # To use the wayland feature
-  ];
-  LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
-}
+  mkShell rec {
+    nativeBuildInputs = [
+      pkg-config
+    ];
+    buildInputs = [
+      udev
+      alsa-lib
+      vulkan-loader
+      xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
+      libxkbcommon wayland # To use the wayland feature
+      cargo
+    ];
+    LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+  }
